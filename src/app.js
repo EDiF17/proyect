3d629ee095a886cc82dc2d23cad3298a7912bfd9
unknown <urielwaigandt9@gmail.com>
2023-11-03 13:06:21 -0300

    acomodando footer

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const methodOverride =  require('method-override');
-
+var session = require("express-session")
 
 // ************ Route System require ************
 const mainRoutes = require('./routes/main');
@@ -26,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
+app.use (session({secret:"secret"}))
 // app.use(logMiddleware);
 
 
